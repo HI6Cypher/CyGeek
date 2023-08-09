3c853ae10df6780139dad0d59fd57202ebe911bb
HI6Cypher <swhwap.net@gmail.com>
2023-08-09 18:22:20 +0430

    CyGeek "establish MainServer and optimazation"

--- client_server/client_server.py
+++ client_server/client_server.py
@@ -2,14 +2,17 @@ import socket
 import threading
 import json
 import re
+import os
 import datetime
 
 
 class ClientServer :
     def __init__(self) :
         self.text = "\n1.Post\n2.Inbox\n3.Sent"
+        self.path_Inbox = f"{os.getcwd()}/data/Inbox.json"
+        self.path_server_info = f"{os.getcwd()}/data/server_info.json"
 
-    def main(self) :
+    def main(self) : #TODO
         task_0 = threading.Thread(target = self.handshake)
         task_0.start()
         task_0.join()
@@ -26,7 +29,7 @@ class ClientServer :
                 task_2.join()
             elif choice == "2" :
                 counter = 1
-                with open("data/Inbox.json", "r") as file :
+                with open(self.path_Inbox, "r") as file :
                     for i in json.load(file) :
                         text = "[%s]\n------------\nTime: %s\nSender: %s\nHostname: %s\nHost: %s\nMessage: %s\n------------\n" \
                             % (counter, i["time"], i["sender"], i["hostname"], i["host"], i["message"])
@@ -36,43 +39,47 @@ class ClientServer :
     def handshake(self) :
         server = dict()
         payload = f"Hi i am [{socket.gethostbyname(socket.gethostname())}]"
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as handshake :
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as handshake :
+            handshake.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             try :
-                with open("data/server_info.json", "r") as file :
+                with open(self.path_server_info, "r") as file :
                     info = json.load(file)
             except :
-                for i in range(2, 256) :
-                    handshake.connect((f"192.168.43.{i}", 50321))
-                    handshake.sendall(payload.encode())
-                    data = handshake.recv(1024)
-                    if "Hi" in data.decode() :
-                        try :
-                            server_host = re.search(r"(?<=HOST:).+(?=:HOST)", data.decode()).group()
-                            server_port = re.search(r"(?<=PORT:).+(?=:PORT)", data.decode()).group()
-                        except :
-                            print("[!] security problem in network".upper())
-                            break
-                        else :
-                            server["server_host"] = server_host
-                            server["server_port"] = server_port
-                            with open("data/server_info.json", "w") as file :
-                                json.dump(server, file)
+                    try :
+                        handshake.settimeout(2)
+                        handshake.sendto(payload.encode(), ("255.255.255.255", 50321))
+                        handshake.settimeout(10)
+                        data = handshake.recvfrom(1024)
+                        print(f"[*] the server found {data[1]}".upper())
+                    except :
+                        pass
                     else :
-                        continue
+                        if "Hi" in data[0].decode() :
+                            try :
+                                server_host = re.search(r"(?<=HOST:).+(?=:HOST)", data[0].decode()).group()
+                                server_port = re.search(r"(?<=PORT:).+(?=:PORT)", data[0].decode()).group()
+                            except :
+                                print("[!] security problem in network".upper())
+                            else :
+                                server["server_host"] = server_host
+                                server["server_port"] = server_port
+                                with open(self.path_server_info, "w") as file :
+                                    json.dump([server], file)
+                        else :
+                            pass
             else :
-                handshake.connect((info["server_host"], int(info["server_port"])))
+                handshake.connect((info[0]["server_host"], 50321))
                 handshake.sendall(payload.encode())
 
     def listen(self) :
-        path = "data/Inbox.json"
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as net :
-            net.bind((socket.gethostbyname(socket.gethostname()), 60321))
-            net.listen(2)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen :
+            listen.bind((socket.gethostbyname(socket.gethostname()), 60321))
+            listen.listen(5)
             while True :
-                conn, _ = net.accept()
+                conn, _ = listen.accept()
                 with conn :
                     data = conn.recv(1024)
-                    self.saver(path, data)
+                    self.saver(self.path_Inbox, data)
 
     def post(self, hostname, message) :
         time = datetime.datetime.today()
@@ -85,13 +92,14 @@ class ClientServer :
             }
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as net :
             try :
-                with open("data/server_info.json", "r") as file :
+                with open(self.path_server_info, "r") as file :
                     info = json.load(file)
-                net.connect((info["server_host"], 40321)) #TODO
+                net.connect((info[0]["server_host"], 40321)) #TODO
                 net.sendall(json.dumps(packet).encode())
             except OSError as error :
                 error = " ".join(str(error).split()[2:])
-                print(f"[*] Error!--> {error}")
+                print(f"[POST] Error!--> {error}")
+
 
     def saver(self, path, data) : #TODO
         try :
@@ -108,3 +116,7 @@ class ClientServer :
                 loader.append(json.loads(data.decode()))
             with open(path, "w") as file :
                 json.dump(loader, file, indent = 4)
+
+if __name__ == "__main__" :
+    client = ClientServer()
+    client.main()
--- main_server/main_server.py
+++ main_server/main_server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import json
+import os
 
 #handshake to response client handshake : when a client handshakes to server, the server checks unsend
 #messages file and if found unsend message about the client, will send it again.
@@ -12,58 +13,84 @@ import json
 #saver : to save unsend messages.
 class MainServer :
     def __init__(self) :
-        pass
+        self.text = """\nmainserver\n"""
+        self.path_unsends = f"{os.getcwd()}/data/unsends.json"
 
     def main(self) :
-        pass
+        if not os.path.exists(self.path_unsends) :
+            first = [{
+                    "time": "01/01/1970--00:00:00",
+                    "sender": "None",
+                    "hostname": "None",
+                    "host": "None",
+                    "message": "MainServer"
+                }]
+            with open(self.path_unsends, "x") as file :
+                json.dump(first, file, indent = 4)
+        task_0 = threading.Thread(target = self.handshake)
+        task_0.start()
+        task_1 = threading.Thread(target = self.listen)
+        task_1.start()
+        print(self.text)
+        input("Press anykey to setup the Server... ")
+
 
     def handshake(self) :
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as handshake :
+        payload = "Hi [HOST:%s:HOST][PORT:40321:PORT]" % (socket.gethostbyname(socket.gethostname()))
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as handshake :
             handshake.bind((socket.gethostbyname(socket.gethostname()), 50321))
-            handshake.listen(5)
             while True :
-                _, address = handshake.accept()
-                with address :
-                    with open("data/unsend.json", "r") as file :
+                try :
+                    conn, address = handshake.recvfrom(1024)
+                    print(address)
+                    handshake.sendto(payload.encode(), address)
+                    with open(self.path_unsends, "r") as file :
                         unsends = json.load(file)
-                    for unsend in unsends :
+                    for unsend in unsends : #TODO to remove unsend message
                         if unsend["host"] == address[0] :
                             self.post(address[0], unsend, address)
+                            unsends.remove(unsend)
+                            with open(self.path_unsends, "w") as file :
+                                json.dump(unsends, file, indent = 4)
                         else :
                             continue
+                except OSError as error:
+                    continue
+                except Exception as error :
+                    print(f"[HANDSHAKE] Error!--> {error}")
+                    continue
 
     def listen(self) :
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listen :
             listen.bind((socket.gethostbyname(socket.gethostname()), 40321))
             listen.listen(5)
             while True :
-                conn, address = listen.accept()
-                with conn :
-                    data = conn.recv(1024)
-                    if not data :
-                        payload = "your message is not recognized".upper()
-                        listen.sendto(payload, address)
-                    else :
-                        data = json.loads(data.decode())
-                        self.post(data["host"], data, address)
+                try :
+                    conn, address = listen.accept()
+                    with conn :
+                        data = conn.recv(1024)
+                        if not data :
+                            payload = "your message is not recognized".upper()
+                            conn.send(payload)
+                        else :
+                            data = json.loads(data.decode("utf-8"))
+                            self.post(data["host"], data, address)
+                except Exception as error:
+                    print(f"[LISTEN] Error!--> {error}")
 
     def post(self, host, payload, address) :
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as post :
             try :
                 post.connect((host, 60321))
                 post.sendall(json.dumps(payload).encode())
-                delivery_report = f"[+] {address[0]} is online and received your message".encode()
-                post.sendto(delivery_report, address)
             except :
-                delivery_report = f"[*] {address[0]} isn't online but will receive your message".encode()
-                post.sendto(delivery_report, address)
-                self.saver("data/unsend.json", payload)
+                pass
 
     def saver(self, path, data) : #TODO
         try :
             with open(path, "r") as file :
                 loader = json.load(file)
-                loader.append(json.loads(data.decode()))
+                loader.append(json.loads(data))
             with open(path, "w") as file :
                 json.dump(loader, file, indent = 4)
         except FileNotFoundError :
@@ -71,6 +98,10 @@ class MainServer :
                 json.dump([], file)
             with open(path, "r") as file :
                 loader = json.load(file)
-                loader.append(json.loads(data.decode()))
+                loader.append(json.loads(data))
             with open(path, "w") as file :
                 json.dump(loader, file, indent = 4)
+
+if __name__ == "__main__" :
+    server = MainServer()
+    server.main()
